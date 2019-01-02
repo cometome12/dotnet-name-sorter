2c044dedf93eaa524af1a8a4074a2c56ba952706
cometome12 <yangfei9310@gmail.com>
2019-01-03 06:52:33 +1100

    Add another test and test name file

--- NameSorter.Tests/UnitTest1.cs
+++ NameSorter.Tests/UnitTest1.cs
@@ -6,11 +6,15 @@ namespace NameSorter.Tests
 {
     public class UnitTest1
     {
-    	private readonly  Validator validator;
+    	private readonly Validator validator;
+        private readonly File file;
+        private readonly PersonList people;
 
     	public UnitTest1()
     	{
     		this.validator = new Validator();
+            this.file = new File();
+            this.people = new PersonList();
     	}
     	
         [Fact]
@@ -18,6 +22,20 @@ namespace NameSorter.Tests
         {
             string[] args = {"inputa","input"};
         	Assert.False(validator.argsNumberOne(args));
+        }
+
+        [Fact]
+        public void TestAgainst1000Names()
+        {
+            file.read("../../../../test-names-list.txt");
+            file.mapNameToPersonList(people);
+            people.sortByLastNameAndGivenNames();
+            string[] expected = {"Ah Ara", "Ba Aca Lava", "James Wob Parsons", 
+            "James Wow Parsons", "Janet Parsons", "Janet Wob Parsons", 
+            "Janet Wow Parsons", "John Parsons", "John Pooobo", "Wood Pooobo",
+            "Colin Wise"};
+            Assert.Equal(expected, people.getFullNameArray());
+           
         }
     }
 }
